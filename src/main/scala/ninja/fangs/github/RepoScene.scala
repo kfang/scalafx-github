969ed41386c28ff6b0d88ed2d8a740392b296c53
fangs <kevin@zipfworks.com>
2014-07-29 15:16:44 -0700

    adding issues

--- src/main/scala/ninja/fangs/github/RepoScene.scala
+++ src/main/scala/ninja/fangs/github/RepoScene.scala
@@ -1,29 +1,40 @@
 package ninja.fangs.github
 
+import org.eclipse.egit.github.core.Repository
 import org.eclipse.egit.github.core.client.GitHubClient
 import org.eclipse.egit.github.core.service.{IssueService, RepositoryService}
+import scalafx.collections.ObservableBuffer
+import scalafx.geometry.{Insets, Pos}
 import scalafx.scene.Scene
-import scalafx.scene.control.TabPane.TabClosingPolicy
-import scalafx.scene.control.{TabPane, Tab}
+import scalafx.scene.control._
 import scalafx.scene.layout.{VBox, Priority}
 import scala.collection.JavaConversions._
+import scalafx.Includes._
+import scalafx.util.StringConverter
 
 class RepoScene(client: GitHubClient, repos: RepositoryService) extends Scene {
 
-  IssuesConsole.stage.width = 800
-  IssuesConsole.stage.height = 800
   stylesheets = Seq("caspian.css")
 
-  val repoTabs = repos.getRepositories.map(r => {
-    new Tab {
-      text = r.getName
-      content = new VBox()
-    }
-  })
+  val issuesList = new VBox {
+    content = Seq()
+  }
 
-  root = new TabPane {
-    tabs = repoTabs
-    tabClosingPolicy = TabClosingPolicy.UNAVAILABLE
+  val reposList = ObservableBuffer[Repository](repos.getRepositories)
+  val repoDropDown = new ChoiceBox[Repository](reposList){
+    alignmentInParent = Pos.TOP_LEFT
+    converter = StringConverter.toStringConverter[Repository](repo => repo.getName)
+    selectionModel().selectedItem.onChange((value, _, _) => {
+      val issueClient = new IssueService(client)
+      val issues = issueClient.getIssues(value.value, Map[String, String]())
+      issuesList.content = issues.map(issue => new Label(issue.getTitle))
+    })
+  }
+
+  root = new VBox {
+    content = Seq(repoDropDown, issuesList)
+    spacing = 10
+    padding = Insets(20)
     vgrow = Priority.ALWAYS
     hgrow = Priority.ALWAYS
   }
