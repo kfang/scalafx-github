323795c58abc9e775166c1aadf197dd226df03d9
fangs <kevin@zipfworks.com>
2014-07-30 14:22:13 -0700

    figured out the organizations issue

--- src/main/scala/ninja/fangs/github/RepoScene.scala
+++ src/main/scala/ninja/fangs/github/RepoScene.scala
@@ -2,12 +2,13 @@ package ninja.fangs.github
 
 import org.eclipse.egit.github.core.Repository
 import org.eclipse.egit.github.core.client.GitHubClient
-import org.eclipse.egit.github.core.service.{IssueService, RepositoryService}
+import org.eclipse.egit.github.core.service.{OrganizationService, IssueService, RepositoryService}
 import scalafx.collections.ObservableBuffer
+import scalafx.event.ActionEvent
 import scalafx.geometry.{Insets, Pos}
 import scalafx.scene.Scene
 import scalafx.scene.control._
-import scalafx.scene.layout.{VBox, Priority}
+import scalafx.scene.layout.{HBox, VBox, Priority}
 import scala.collection.JavaConversions._
 import scalafx.Includes._
 import scalafx.util.StringConverter
@@ -31,8 +32,39 @@ class RepoScene(client: GitHubClient, repos: RepositoryService) extends Scene {
     })
   }
 
+  val userRepoSearchBox = new HBox {
+
+    val userTextField = new TextField {
+      promptText = "user"
+      text = client.getUser
+    }
+    val typeDropDown = new ChoiceBox[String](ObservableBuffer[String](Seq("user", "organization"))){
+      selectionModel().selectFirst()
+    }
+
+    val userSubmitBtn = new Button {
+      text = "submit"
+      defaultButton = true
+      onAction = (e: ActionEvent) => typeDropDown.selectionModel().getSelectedItem match {
+        case "user"         =>
+          val userRepos = repos.getRepositories(userTextField.text.value)
+          reposList.clear()
+          reposList.++=(userRepos)
+          repoDropDown.selectionModel().selectFirst()
+        case "organization" =>
+          val orgRepos = repos.getOrgRepositories(userTextField.text.value)
+          reposList.clear()
+          reposList.++=(orgRepos)
+          repoDropDown.selectionModel().selectFirst()
+      }
+    }
+
+    content = Seq(userTextField, typeDropDown, userSubmitBtn)
+    spacing = 10
+  }
+
   root = new VBox {
-    content = Seq(repoDropDown, issuesList)
+    content = Seq(userRepoSearchBox, repoDropDown, issuesList)
     spacing = 10
     padding = Insets(20)
     vgrow = Priority.ALWAYS
@@ -40,3 +72,5 @@ class RepoScene(client: GitHubClient, repos: RepositoryService) extends Scene {
   }
 
 }
+
+
